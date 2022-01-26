029746a04f73c5318626f5e6ddb7ebeb78a78fa3
jinu12 <kzkju@naver.com>
2022-01-26 10:02:58 +0900

    two practice

---
+++ basic/inoutputtest.py
@@ -0,0 +1,7 @@
+# 표준입력
+num1 = int(input("숫자 1"))
+num2 = int(input("숫자 2"))
+
+# 표준출력#
+print("입력 숫자 1 : ", num1)
+print("입력 숫자 2 : ", num2)
--- exam/ifExam01.py
+++ exam/ifExam01.py
@@ -1,6 +1,5 @@
-'''
-  입력받은 값을 평가하여 값을 출력하시요
-
+"""
+    입력 받은 값을 평가하여 값을 출력하시요
   [조건]
   - 나이와 성별을 각각 입력받는다. (변수명은 임의로 정의)
   - 입력받은 아이와 성별을 출력하기
@@ -9,22 +8,24 @@
     2) 나이가 20이상 성별 2 => 성인 여자
     3) 나이가 20미만 성별 1 => 청소년 남자
     4) 나이가 20미만 성별 2 => 청소년 여자
-'''
-case = int(input('세대 구문 : 1, 종료 : 2\n'))
-if case == 1:
-    age = int(input('나이를 입력해주세요 : '))
-    human = int(input('성별을 입력해수제요 : '))
-    if age >= 20:
-        if human == 1:
-            print("성인 남자")
-        if human == 2:
-            print("성인 여자")
-    else:
-        if human == 1:
-            print("청소년 남자")
-        if human == 2:
-            print("청소년 여자")
-else:
-    print("종료되었습니다.")
-    case = 2
+"""
 
+test = bool
+while test:
+    case = int(input('세대 구문 : 1, 종료 : 2\n'))
+    if case == 1:
+        age = int(input('나이를 입력해주세요 : '))
+        human = int(input('성별을 입력해수제요 : '))
+        if age >= 20:
+            if human == 1:
+                print("성인 남자")
+            elif human == 2:
+                print("성인 여자")
+        else:
+            if human == 1:
+                print("청소년 남자")
+            elif human == 2:
+                print("청소년 여자")
+    elif case == 2:
+        print("종료되었습니다.")
+        test = False
