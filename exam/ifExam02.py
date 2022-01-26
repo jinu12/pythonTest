11986ff1911ca02a68723825971f8d7213d130e7
jinu12 <kzkju@naver.com>
2022-01-26 13:29:27 +0900

    six commit

--- exam/ifExam02.py
+++ exam/ifExam02.py
@@ -11,6 +11,7 @@
     숫자1 : 0
     입력한 숫자는 0이며 제로입니다.
 """
+'''
 test = bool
 while test:
     case = input('숫자 입력 확인 : 1, 종료 : 2\n')
@@ -18,10 +19,28 @@ while test:
         num = int(input(" 숫자 1 : "))
         if num > 0:
             print('입력한 숫자는 %d 이며 양수입니다.' % num)
-        if num < 0:
+        elif num < 0:
             print('입력한 숫자는 %d 이며 음수입니다.' % num)
-        if num == 0:
+        else:
             print('입력한 숫자는 %d 이며 제로입니다.' % num)
     if case == "2":
         print('종료되었습니다.')
         test = False
+'''
+test = bool
+while test:
+    case = input('숫자 입력 확인 : 1, 종료 : 2\n')
+    if case == "1":
+        num = int(input(" 숫자 1 : "))
+        result = ""
+        if num > 0:
+            result = "양수"
+        elif num < 0:
+            result = "음수"
+        else:
+            result = "제로"
+        print("입력한 숫자는 %d 이며 %s입니다" % (num, result))
+    if case == "2":
+        print('종료되었습니다.')
+        test = False
+
