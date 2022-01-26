41980fbc63c6cafec07b35ef7be3e7cb7972ceba
jinu12 <kzkju@naver.com>
2022-01-26 15:15:15 +0900

    eight commit

--- exam/gugu.py
+++ exam/gugu.py
@@ -1,5 +1,4 @@
 # 출력할 단을 입력받아 입력된 구구단을 출력할 수 있도록 작성 출력형태
 num = int(input("단 : "))
-for i in range(1,10):
-    gugu = num * i
-    print("%d * %d = %d" % (num, i, gugu))
+for i in range(1, 10):
+    print("%d * %d = %d" % (num, i, num * i))
--- exam/hapTest.py
+++ exam/hapTest.py
@@ -1,3 +1,4 @@
+# 1부터 100까지의 숫자의 총합 , 홀수합, 짝수합
 Sum = 0
 odd = 0
 even = 0
@@ -10,4 +11,3 @@ for i in range(1, 101):
 print("총합 : %d" % Sum)
 print("홀수합 : %d" % odd)
 print("짝수합 : %d" % even)
-
