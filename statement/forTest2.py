e8e74d45a4fb01fd0872f1133d21617638c03d4d
jinu12 <kzkju@naver.com>
2022-01-27 11:26:26 +0900

    eleven commit

--- statement/forTest2.py
+++ statement/forTest2.py
@@ -3,14 +3,18 @@ for i in range(1, 6):
 
 print("*"*20)
 
-for i in range(5,0,-1):
+for i in range(5, 0, -1):
     print("파이썬 프로그래밍")
 
 # 1부터  100까지의 합에서 3의 배수의 합 구하기
-
+Sum2 = 0
 Sum = 0
 for i in range(0, 101, 3):
     Sum = Sum + i
-    print(i)
-print(Sum)
+    print(Sum)
 
+
+for i in range(1, 100):
+    if i % 3 == 0:
+        Sum2 = Sum2 + i
+print(Sum2)
