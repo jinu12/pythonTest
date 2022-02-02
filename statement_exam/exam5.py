e6b8694390d1bab6800993d489a7d339fa2ef9c6
jinu12 <kzkju@naver.com>
2022-02-03 00:06:57 +0900

    thirty four commit

--- statement_exam/exam5.py
+++ statement_exam/exam5.py
@@ -1,1 +1,13 @@
-#
+# 길이가 10인 리스트 원본과 랜범 번호
+import random
+
+tenlist = list(range(1, 11, 1))
+print("원본 :", end="")
+for i in range(len(tenlist)):
+    print("{},".format(i), end="")
+
+print("")
+
+print("결과 :", end="")
+for i in range(len(tenlist)):
+    print("{},".format(random.randrange(len(tenlist))), end="")
