c9636e751f9a874423b4dd2b1009ee7854438066
jinu12 <kzkju@naver.com>
2022-02-02 23:59:59 +0900

    thirty four commit

--- statement_exam/exam4.py
+++ statement_exam/exam4.py
@@ -1,1 +1,19 @@
 # 주어진 리스트이 최소갑 최빈값 구하기
+answerlist = [79, 88, 91, 33, 100, 55, 95]
+answermaxlist = []
+answerlastlist = []
+count = 0
+answermax = 0
+for i in range(len(answerlist)):
+    for j in range(len(answerlist)):
+        if answerlist[i] < answerlist[j]:
+            count = count + 1
+            answermaxlist.append(answerlist[j])
+for i in range(len(answerlist)):
+    for j in range(len(answerlist)):
+        if answerlist[i] > answerlist[j]:
+            answerlastlist.append(answerlist[j])
+print("최빈값 : {}".format(answermaxlist[count-1]))
+print("최빈값 : {}".format(answerlastlist[count-1]))
+
+
